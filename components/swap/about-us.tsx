299a52ca0e67f2abfc468874307a941024957286
Piyush Das <112721924+piyush-3110@users.noreply.github.com>
2024-12-08 23:56:41 +0530

    Update about-us.tsx(changed font size)

--- components/swap/about-us.tsx
+++ components/swap/about-us.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 export const About = () => {
 	return (
-		<div className="flex  items-center w-full py-24">
+		<div className="flex  items-center w-full ">
 			<div className=" text-center">
-				<h1 className="text-4xl font-semibold mb-6 font-avenixel">About Us</h1>
-				<p className="md:w-[80%] lg:w-[65%] px-4 md:px-20 lg:px-36 text-sm text-left mt-8 font-avenixel text-white">
+				<h1 style="font-size: 63.18px;" className=" font-semibold mb-6 font-avenixel">About Us</h1>
+				<p className="md:w-[80%] lg:w-[65%] px-4 md:px-20 lg:px-36  text-left mt-8 font-avenixel text-white" style="font-size: 23.18px;">
 					<span className="gradient-underline">DEX Raiden</span> is a pioneering
 					decentralized platform revolutionizing payments by offering efficient,
 					transparent, and accessible digital asset exchange. Our system eliminates
